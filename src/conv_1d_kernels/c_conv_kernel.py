cf80f63431a09cf610a9bf1fa0a37ea16b7b55e6
Davide Scintu <davidescintu195@gmail.com>
2022-11-08 09:59:34 +0100

    Added files with classes to implement

--- src/conv_1d_kernels/c_conv_kernel.py
+++ src/conv_1d_kernels/c_conv_kernel.py
@@ -3,4 +3,31 @@ from abc import ABC, abstractmethod
 
 
 class CConvKernel(ABC):
-    pass
+
+    def __init__(self, kernel_size=3):
+        self._mask = None
+        self._kernel_size = kernel_size  # call setter (odd number)
+
+    @property
+    def kernel_size(self):
+        return self._kernel_size
+
+    @kernel_size.setter
+    def kernel_size(self):
+        pass
+
+    @property
+    def mask(self):
+        return self._mask
+
+    @mask.setter
+    def mask(self):
+        pass
+
+    def kernel(self, x):
+        xp = x
+
+
+        return xp
+
+
